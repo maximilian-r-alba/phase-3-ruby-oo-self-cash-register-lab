cb15248bcbaba49f6a0bc53c7a76774d7cc1c535
Maximilian Rex Alba <albam476@gmail.com>
2022-12-10 17:29:32 -0500

    commit tree 263c3abaa813485068f7217b9d0ec51d81bd5faf

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,23 @@
+require 'pry'
 
+class CashRegister
+
+    attr_accessor :discount, :total
+
+    def initialize (discount = 0)
+        @discount = discount
+        @total = 0
+    end
+
+    def add_item item, price, quantity = 1
+        self.total += (price * quantity)
+    end
+
+    def apply_discount
+        self.total = self.total - (self.total * (self.discount/100))
+    end
+
+end
+
+binding.pry
+0
